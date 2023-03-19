00713a3fde3c333abae580510012092fd26443c1
edwilsontj <lmconsultoria@outlook.com.br>
2023-03-19 16:03:27 -0300

    Tabelas iniciais

--- start/routes.ts
+++ start/routes.ts
@@ -1,11 +1,31 @@
 import Route from '@ioc:Adonis/Core/Route'
-import User from 'App/Models/User'
-// return { hello: 'world' }
 
 Route.get('/', async () => {
-  User.create({
-    email: "admin@email.com",
-    password: "123456",
-    tipo: "admin",
-  })
+  // User.create({
+  //   email: 'admin@email.com',
+  //   password: '123456',
+  //   tipo: 'admin',
+  // })
+  return {
+    hortifrutti: "Curso!",
+  }
 });
+
+// Route.post('/gettoken', async ({request, response, auth}: HttpContextContract ) => {
+//   const email = request.input("email");
+//   const password = request.input("password");
+  
+//   const user = await User.findBy("email",email);
+
+//   if (user == null) {
+//     return response.notFound("Usuário não encontrado")
+//   }
+  
+//   const token = await auth.use('api').attempt(email, password);
+
+//   return response.ok(token)
+// });
+
+// Route.get('/auth', async ({ auth, response }: HttpContextContract ) => {
+//   return response.ok("Somente usuários autenticados!")
+// }).middleware("auth");
