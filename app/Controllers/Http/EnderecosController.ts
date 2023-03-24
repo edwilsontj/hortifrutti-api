3d4a87312014587d180fd96cfd7724f69f5ceeb9
edwilsontj <lmconsultoria@outlook.com.br>
2023-03-24 14:33:58 -0300

    Migration - Final

--- app/Controllers/Http/EnderecosController.ts
+++ app/Controllers/Http/EnderecosController.ts
@@ -1,64 +1,16 @@
-import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
-import Cliente from "App/Models/Cliente";
-import Endereco from "App/Models/Endereco";
-import CreateEditEnderecoValidator from "App/Validators/CreateEditEnderecoValidator";
+import BaseSchema from "@ioc:Adonis/Lucid/Schema";
 
-export default class EnderecosController {
-    public async store({ request, response, auth }: HttpContextContract) {
-        const payload = await request.validate(CreateEditEnderecoValidator);
-        const userAuth = await auth.use("api").authenticate();
-        const cliente = await Cliente.findByOrFail("user_id", userAuth.id);
+export default class extends BaseSchema {
+    protected tableName = "statuses";
 
-        const endereco = await Endereco.create({
-            cidadeId: payload.cidade_id,
-            clienteId: cliente.id,
-            rua: payload.rua,
-            numero: payload.numero,
-            bairro: payload.bairro,
-            pontoReferencia: payload.ponto_referencia,
-            complemento: payload.complemento,
+    public async up() {
+        this.schema.createTable(this.tableName, (table) => {
+            table.increments("id").primary();
+            table.string("status", 50).notNullable();
         });
-
-        return response.ok(endereco);
     }
 
-    public async index({ auth, response }: HttpContextContract) {
-        const userAuth = await auth.use("api").authenticate();
-        const cliente = await Cliente.findByOrFail("user_id", userAuth.id);
-
-        const getCliente = await Cliente.query()
-            .where("id", cliente.id)
-            .preload("enderecos", (CidadeQuery) => {
-                CidadeQuery.preload("cidade", (queryEstado) => {
-                    queryEstado.preload("estado");
-                });
-            })
-            .firstOrFail();
-
-        return response.ok(getCliente.enderecos);
-    }
-
-    public async update({ request, response, params }: HttpContextContract) {
-        const payload = await request.validate(CreateEditEnderecoValidator);
-        const endereco = await Endereco.findOrFail(params.id);
-
-        endereco.merge(payload);
-        await endereco.save();
-
-        return response.ok(endereco);
-    }
-
-    public async destroy({ response, params }: HttpContextContract) {
-        try {
-            const resp = await Endereco.query().where("id", params.id).delete();
-
-            if (resp.includes(1)) {
-                return response.noContent();
-            } else {
-                return response.notFound();
-            }
-        } catch {
-            return response.badRequest();
-        }
+    public async down() {
+        this.schema.dropTable(this.tableName);
     }
 }
